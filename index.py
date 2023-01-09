5ee2dbab9e9d995914d8b53fc2dfb15b1508f128
Raghav <gadaparaghavendra24@gmail.com>
2023-01-09 18:15:38 +0530

    save point

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-file = open("sample.txt", "r")
-print(file.read())
+file = open("sample.txt", "r") # if mode is not specified then by default "read" mode will be taken as a mode!!
 file.close()
 # print(file.read()) -> error because file has been closed!!
